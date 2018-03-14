2bbe1856ef1f420c62928cf71953072091edc0fa
Emilie Galambos <emiliegalambos@Emilies-Macbook-Pro.local>
2018-03-14 17:50:34 -0500

    ??

--- showfiler_sales/settings.py
+++ showfiler_sales/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
-STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATIC_URL = os.path.join(os.path.dirname(__file__), 'static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_FROM_EMAIL = 'info@showfiler.com'
